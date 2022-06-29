d348738482b1fdd4d19aa910878b56b99f25fafe
Genaro Zottele <genarozottele14@gmail.com>
2022-06-29 01:05:12 -0300

    30425zottele

---
+++ js_practica.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+let numero = prompt("Ingresa un numero");
+
+for( i = 1; i <= 10; i++){
+    let resultado = numero*i;
+    console.log(numero + " X " + i + " es igual a " + resultado);
+}
+
+
+
+// ---------------------------------------------------------------------
+
+
+
+// let a = parseInt(prompt("lado a"));
+// let b = parseInt(prompt("lado b"));
+// let c = parseInt(prompt("lado c"));
+
+// while (a + b >= c || a + c >= b || c + b >= a) {
+//     if ( (a == b) && (b == c) ){
+//         console.log("Es equilatero");
+//     }else if ( (a == b) || (b == c) || (c == a) ){
+//         console.log("Es isosceles");
+//     }else{
+//         console.log("Es escaleno");
+//     }
+//     a = parseInt(prompt("nuevo lado a"));
+//     b = parseInt(prompt("nuevo lado b"));
+//     c = parseInt(prompt("nuevo lado c"));
+// }
